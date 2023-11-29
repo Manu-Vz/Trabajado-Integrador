82bb7c85cf75aee331a37af5ac3de90cd8a5a7d9
Manuel Vasquez <manuvasquez2004@gmail.com>
2023-11-29 18:11:11 -0300

    Mas cambios y correcciones

--- trabajoIntegradorMaven/src/main/java/Programa/Servicio.java
+++ trabajoIntegradorMaven/src/main/java/Programa/Servicio.java
@@ -12,4 +12,34 @@ public class Servicio {
     private String nombre;
     private String description;
     private List<TipoProblema> tipoDeProblemas;
+
+    public Servicio(String nombre, String description, List<TipoProblema> tipoDeProblemas) {
+        this.nombre = nombre;
+        this.description = description;
+        this.tipoDeProblemas = tipoDeProblemas;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<TipoProblema> getTipoDeProblemas() {
+        return tipoDeProblemas;
+    }
+
+    public void setTipoDeProblemas(List<TipoProblema> tipoDeProblemas) {
+        this.tipoDeProblemas = tipoDeProblemas;
+    }
 }
